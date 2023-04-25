d3e9f2678147da174fb2607a8e952beabca013b3
nehajoshi-ctrl <nehajosh@andrew.cmu.edu>
2023-04-25 18:51:44 -0400

    api finsihed off
    finished api shit
    dont run refresh unless you really have to

--- shoptaki/models.py
+++ shoptaki/models.py
@@ -6,20 +6,20 @@ from datetime import datetime
 
 
 class Listing(models.Model):
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=20)
+    address = models.CharField(max_length=200, default = "5000 Forbes Ave")
+    city = models.CharField(max_length=100, default="Pittsburgh")
+    state = models.CharField(max_length=100, default="PA")
+    zipcode = models.CharField(max_length=20, default=-1)
     price = models.IntegerField()
-    bedrooms = models.IntegerField()
-    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
-    sqft = models.IntegerField()
-    lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    days_listed = models.IntegerField()
-    longitude = models.DecimalField()
-    latitude = models.DecimalField()
-    img = models.CharField(max_length=200)
-    rent_estimate = models.IntegerField()
+    bedrooms = models.IntegerField(default=-1)
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1, default=-1)
+    sqft = models.IntegerField(default=-1)
+    lot_size = models.DecimalField(max_digits=5, decimal_places=1, default =-1)
+    days_listed = models.IntegerField(default=-1)
+    longitude = models.DecimalField(max_digits=12, decimal_places=9, default=-1)
+    latitude = models.DecimalField(max_digits=12, decimal_places=9, default=-1)
+    img = models.CharField(max_length=200, default="")
+    rent_estimate = models.IntegerField(default = -1)
 
     def __str__(self):
         return self.title
